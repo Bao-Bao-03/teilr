2734729194ae3d03c841a4ea68ffcf8f63e6f928
Bảo Trần <10423013@student.vgu.edu.vn>
2026-07-04 21:21:27 +0200

    Update BillCreateRequest.java

--- src/main/java/frauas/teilr/dto/BillCreateRequest.java
+++ src/main/java/frauas/teilr/dto/BillCreateRequest.java
@@ -7,9 +7,9 @@ import java.util.List;
 @Data
 public class BillCreateRequest {
     private Long groupId;
-    private Long creatorId; // ID người móc ví trả tiền
+    private Long creatorId;
     private String description;
     private BigDecimal totalAmount;
-    private List<Long> participantIds; // Mảng ID những người ăn chung
-    private String participantNames; // Chuỗi tên để UI dễ bề hiển thị
+    private List<Long> participantIds;
+    private String participantNames;
 }
