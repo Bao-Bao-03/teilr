29518fc8bebcb1cae95f156e44d2802a8e87efe9
Bảo Trần <10423013@student.vgu.edu.vn>
2026-07-04 21:22:39 +0200

    Update Bill.java

--- src/main/java/frauas/teilr/entity/Bill.java
+++ src/main/java/frauas/teilr/entity/Bill.java
@@ -7,14 +7,14 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-@Table(name = "bills") //chỉ định chính xác tên bảng dưới MySQL.
-@Data // tự sinh ngầm toàn bộ Getters, Setters, equals(), hashCode() và toString() trong lúc biên dịch
-@NoArgsConstructor //tự động tạo ra một Constructor rỗng (không có tham số truyền vào)
+@Table(name = "bills")
+@Data
+@NoArgsConstructor
 
 public class Bill {
 
-    @Id //đánh dấu thuộc tính ngay bên dưới nó chính là Khóa chính (Primary Key) của bảng MySQL.
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //quy định Cách thức tạo ra cái @Id đó.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "group_id", nullable = false)
@@ -26,7 +26,7 @@ public class Bill {
     @Column(nullable = false)
     private String description;
     
-    @Column(precision = 10, scale = 2) //Tổng số lượng chữ số tối đa, Trong 10 chữ số đó, có đúng 2 chữ số nằm ở phía sau dấu phẩy.
+    @Column(precision = 10, scale = 2)
     private BigDecimal totalAmount;
     
     private String currency = "EUR";
@@ -34,7 +34,6 @@ public class Bill {
     @Column(name = "created_at", updatable = false)
     private Instant createdAt = Instant.now();
     
-    // Cột "Hack" để hiển thị tên người tham gia lên UI cho lẹ 
     @Column(name = "participant_names")
     private String participantNames; 
 
